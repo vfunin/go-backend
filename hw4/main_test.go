e7dcdd1ae9960caf5d7e3a160f3700174ffa88a5
vfunin <i@vfunin.ru>
2022-05-09 20:13:32 +0300

    task 2 - list files extension filter

--- hw4/main_test.go
+++ hw4/main_test.go
@@ -67,6 +67,36 @@ func TestListHandler(t *testing.T) {
 	}
 }
 
+func TestListExtHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/upload?ext=png", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "ok!")
+	}))
+
+	defer ts.Close()
+
+	uploadHandler := &UploadHandler{
+		UploadDir: "upload",
+		HostAddr:  ts.URL,
+	}
+	uploadHandler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	expected := ``
+
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+	}
+}
+
 func TestUploadHandler(t *testing.T) {
 	file, _ := os.Open("test.txt")
 	defer file.Close()
